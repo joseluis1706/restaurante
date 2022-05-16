08786f507bcd444e3c54f4edadbd10d4f7b5a207
José Luis Martínez Navarro <jose.martinez1@softwareone.com>
2022-05-16 15:38:40 -0500

    Proyecto

---
+++ restaurante/restaurante/Datos/DatosPlatos.cs
@@ -0,0 +1,36 @@
+﻿using restaurante.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace restaurante.Datos
+{
+    public class DatosPlatos
+    {
+        public ObservableCollection<ModelPlatos> ListarPlatos()
+        {
+            return new ObservableCollection<ModelPlatos>()
+            {
+                new ModelPlatos()
+                {
+                    descripcion = "plato 1",
+                    precio = "$5.000",
+                    icono = "https://ibb.co/2snsxj1"
+                },
+                new ModelPlatos()
+                {
+                    descripcion = "plato 2",
+                    precio = "$5.000",
+                    icono = "https://ibb.co/MfHBhZ2"
+                },
+                new ModelPlatos()
+                {
+                    descripcion = "plato 3",
+                    precio = "$5.000",
+                    icono = "https://ibb.co/pvS4b2X"
+                },
+            };
+        }
+    }
+}
---
+++ restaurante/restaurante/Model/ModelPlatos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace restaurante.Model
+{
+    public class ModelPlatos
+    {
+        public string descripcion { get; set; }
+        public string precio { get; set; }
+        public string icono { get; set; }
+    }
+}
