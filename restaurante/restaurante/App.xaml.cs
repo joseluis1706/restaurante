5f04304bb7beda83e15c93c082b38073a2190fb3
José Luis Martínez Navarro <jose.martinez1@softwareone.com>
2022-05-16 15:20:18 -0500

    Proyecto

---
+++ restaurante/restaurante/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using restaurante.View.MenuPrincipal;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace restaurante
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Principal();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
