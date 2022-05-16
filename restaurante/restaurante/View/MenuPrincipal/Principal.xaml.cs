87efb957d121a2b5441c354e56f1b62ed2257eef
José Luis Martínez Navarro <jose.martinez1@softwareone.com>
2022-05-16 15:15:39 -0500

    Inicio

---
+++ restaurante/restaurante/View/MenuPrincipal/Principal.xaml.cs
@@ -0,0 +1,22 @@
+﻿using restaurante.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace restaurante.View.MenuPrincipal
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Principal : ContentPage
+    {
+        public Principal()
+        {
+            InitializeComponent();
+            BindingContext = new PrincipalViewModel(Navigation);
+        }
+    }
+}
---
+++ restaurante/restaurante/ViewModel/PrincipalViewModel.cs
@@ -0,0 +1,49 @@
+﻿using restaurante.Datos;
+using restaurante.Model;
+using restaurante.VistaModelo;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace restaurante.ViewModel
+{
+    public class PrincipalViewModel:BaseViewModel
+    {
+        #region VARIABLES
+        ObservableCollection<ModelPlatos> _listaPlatos;
+        #endregion
+        #region CONSTRUCTOR
+        public PrincipalViewModel(INavigation navigation)
+        {
+            Navigation = navigation;
+            ListarPlatos();
+        }
+        #endregion
+        #region OBJETOS
+        public ObservableCollection<ModelPlatos> ListaPlatos
+        {
+            get { return _listaPlatos; }
+            set { SetValue(ref _listaPlatos, value); }
+        }
+        #endregion
+        #region PROCESOS
+        public void ListarPlatos()
+        {
+            var funcion = new DatosPlatos();
+            ListaPlatos = funcion.ListarPlatos();
+        }
+        public void ProcesoSimple()
+        {
+
+        }
+        #endregion
+        #region COMANDO
+        //public ICommand ProcesoAsyncommand => new Command(async () => await ProcesoAsyncrono());
+        public ICommand ProcesoSimpcommand => new Command(ProcesoSimple);
+        #endregion
+    }
+}
